a649a5494ce1d4324c1e9e680fec17376cac8f01
oguzk <oguzcankilickaya@gmail.com>
2023-03-21 00:26:34 +0300

    End Of Section 3

---
+++ client-app/src/App.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Header } from 'semantic-ui-react';
+import List from 'semantic-ui-react/dist/commonjs/elements/List';
+import Button from 'semantic-ui-react/dist/commonjs/elements/Button';
+
+function App() {
+  const [activities , setActivities] = useState([]);
+
+  useEffect(() =>{
+    axios.get('http://localhost:5000/api/activities')
+    .then(response =>{
+      //console.log(response);
+      setActivities(response.data);
+    })
+  },[])
+
+  return (
+    <div className="App">
+       <Header as='h2' icon = "users" content = 'Reactivities'/>
+        <List>
+          {activities.map((activity : any) =>(
+            <List.Item key={activity.id}>
+               {activity.title}             
+            </List.Item>
+          ))}
+        </List>
+        <Button>Test</Button>
+
+    </div>
+  );
+}
+
+export default App;

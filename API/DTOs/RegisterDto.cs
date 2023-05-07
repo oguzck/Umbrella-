0eaa316acde42dda4d193f628cf5432a604e59e6
oguzk <oguzcankilickaya@gmail.com>
2023-05-07 17:04:16 +0300

    End Of Identity

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$",ErrorMessage = "Password must be complex ")]
+        public  string Password { get; set; }
+        [Required]
+        public string DisplayName { get; set; }
+        public string UserName { get; set; }
+    }
+}
--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -1,11 +1,21 @@
 using Domain;
+using Microsoft.AspNetCore.Identity;
 
 namespace Persistence
 {
     public class Seed
     {
-        public static async Task SeedData(DataContext context)
+        public static async Task SeedData(DataContext context , UserManager<AppUser> userManager)
         {
+            if(!userManager.Users.Any()){
+                var users = new List<AppUser>{
+                    new AppUser{DisplayName = "Bilal Göksu" , UserName = "mrLesh" , Email="bilalgoksu6@gmail.com"},
+                    new AppUser{DisplayName = "Cemil Okan Kaşık" , UserName = "DLover" , Email="cemilkasikci6@gmail.com"}
+                };
+                foreach(var user in users){
+                    await userManager.CreateAsync(user,"Pa$$w0rd");
+                }
+            }
             if (context.Activities.Any()) return;
             
             var activities = new List<Activity>
